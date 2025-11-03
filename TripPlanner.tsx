1c677fed99ba9e04dc0a182535fcfdd40d890104
flartist59 <andrewrjr59@gmail.com>
2025-11-03 10:57:31 -0500

    Update TripPlanner.tsx

--- TripPlanner.tsx
+++ TripPlanner.tsx
@@ -113,7 +113,7 @@ const TripPlanner: React.FC = () => {
             <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
               <div className="bg-white rounded-lg max-w-md w-full max-h-[90vh] overflow-y-auto">
                 <StripeCheckout
-                  amount={1999}
+                  amount={799}
                   onSuccess={handlePaymentSuccess}
                   onCancel={() => setShowPayment(false)}
                 />
