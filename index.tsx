f1fad80e17ea7ebdea9e280ed23bb31d15815b9c
flartist59 <andrewrjr59@gmail.com>
2025-11-03 20:30:58 -0500

    Removed index.css import

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx'; // This is your main App component
-import './index.css'; // Your global CSS imports
 import { AuthProvider } from './AuthContext'; // Import AuthProvider (path relative to index.tsx)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
